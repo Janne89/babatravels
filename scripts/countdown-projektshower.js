a7bfe084f1fb40e5aca41754eeec2dc47ad82cd1
Janne <jan_issa@hotmail.com>
2026-06-21 17:27:08 +0200

    add detaljer

--- scripts/countdown-projektshower.js
+++ scripts/countdown-projektshower.js
@@ -1,5 +1,5 @@
 // Ändra datumet nedan till rätt dag/tid för babyshowern!
-const showerDatum = new Date("2026-07-04T10:30:00");
+const showerDatum = new Date("2026-06-27T10:30:00");
 
 function uppdateraNedrakning() {
   const nu = new Date();
